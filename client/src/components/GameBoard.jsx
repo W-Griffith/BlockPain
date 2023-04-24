e669d69ac2fc645d8a0c79782d862979bb038b45
Wesh Griffith <46984407+W-Griffith@users.noreply.github.com>
2023-04-24 15:17:42 -0600

    Updating the Title
    Updated the title and added some basic Component framework

---
+++ client/src/components/GameBoard.jsx
@@ -0,0 +1,11 @@
+function GameBoard() {
+    // const [count, setCount] = useState(0)
+   
+     return (
+       
+      <h1>GameBoard</h1>
+   
+     )
+   }
+   
+   export default GameBoard
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+function NavBar() {
+   
+   
+     return (
+       
+       <h1>Navbar</h1>
+   
+     )
+   }
+   
+   export default NavBar
