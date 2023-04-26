5da5c1bc2aca186d1493d732a9e0f2faf2eecf18
Jack Sovereign <86751863+MarioEmporium@users.noreply.github.com>
2023-04-26 17:09:05 -0600

    Added functionality for a player to join the game and make a move. Extensively tested using remix. Emits correct events. Prevents players from playing out of order.

--- smart_contracts/contracts/TicTacToe.sol
+++ smart_contracts/contracts/TicTacToe.sol
@@ -7,13 +7,15 @@ pragma solidity ^0.8.10;
 contract TicTacToe{
 
     uint public gameNumber = 0;
+    
 
     struct GameState{
         uint gameId;
         address playerOne;
         address playerTwo;
+        bool gameFull;
         PlayerInfo[3][3] board;
-        PlayerInfo nextTurn;
+        address nextTurn;
         PlayerInfo winner;
     }
 
@@ -28,8 +30,9 @@ contract TicTacToe{
 
     function startNewGame() public {
         GameState memory game;
-        game.nextTurn = PlayerInfo.Player1;
+        game.nextTurn = msg.sender;
         game.gameId = gameNumber;
+        game.playerOne = msg.sender;
         games.push(game);       //Games at index i will be the ith game played
         gameNumber++;
 
@@ -38,7 +41,57 @@ contract TicTacToe{
     }
 
 
+    //Adds a second player to the game. Player1 will always be the one who started the game.
+    //Player2 will be the one who calls this function.
+    function addPlayer(uint gameID) public returns (bool flag){
+        address newPlayer = msg.sender;
+        GameState storage game = games[gameID];
+
+        if(game.gameFull){
+            return false;
+        }
+
+        game.playerTwo = newPlayer;
+        game.gameFull = true;
+        emit GameFull(gameID, game.playerOne, game.playerTwo);
+        return true;
+    }
+
+    function makeMove(uint row, uint col, uint gameID) public returns (bool success){
+        GameState storage game = games[gameID];
+
+        if(game.nextTurn != msg.sender){
+            return false;   //Not your turn!
+        }
+        if(game.gameFull == false){
+            return false;   //Need two players before making a move
+        }
+        if(row > 2 || col > 2 || row < 0 || col < 0){
+            return false;   //Out of bounds
+        }
+        if(game.board[row][col] != PlayerInfo.None){      
+            return false;   //Not an empty spot
+        }
+
+        PlayerInfo currentPlayer;
+        if(game.playerOne == msg.sender){
+            currentPlayer = PlayerInfo.Player1;
+        }else{
+            currentPlayer = PlayerInfo.Player2;
+        }
+
+        game.board[row][col] = currentPlayer;
+        emit MoveMade(gameID, row, col);
+        return true;
+
+        //TODO : Check for a winner (probably should be a seperate function call)
+
+    }
+
+
     event NewGame(address playerOne, uint gameID);
+    event GameFull(uint gameID, address playerOne, address playerTwo);
+    event MoveMade(uint gameID, uint row, uint col);
 
 
 
