3791d3deec8dab5a0e7aa4ab2eb8f02783666486
Wesh Griffith <46984407+W-Griffith@users.noreply.github.com>
2023-05-01 10:38:45 -0600

    Adding web3 functionality
    Adding more web3 stuff and testing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,22 @@
 import { Scoreboard, GameBoard, Controls, NavBar } from './components'
 import './App.css'
 
+import { ethers } from 'hardhat';
+import contractABI from './ticTacToeABI.json';
+
+const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+
+const demoGame = async () => {
+
+  const metaMask = new ethers.providers.Web3Provider(window.ethereum);
+  const contract = await hre;
+
+  const firstGame = await contract.method.startNewGame();
+
+  alert(firstGame);
+
+
+}
 
 function App() {
  // const [count, setCount] = useState(0)
@@ -14,6 +30,7 @@ function App() {
       <Controls />
       <Scoreboard />
 
+      <button onClick={demoGame}> Click me!</button>
 
     </div>
 
