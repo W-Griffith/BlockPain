cc7d284c97748e552b798acc6dd57bda41569f69
Sam Howard <samhow@colostate.edu>
2023-05-06 16:43:26 -0600

    alternate x n o

--- client/src/components/GameBoard.jsx
+++ client/src/components/GameBoard.jsx
@@ -3,37 +3,46 @@ import { useState } from 'react';
 
 export default function GameBoard() {
      // const [count, setCount] = useState(0)
-   
+     const [xIsNext, setXIsNext] = useState(true);
+      
      return( 
         <>
         <h1>GameBoard</h1>
         <div className="board-row">
-        <Square coordinate={[0,0]}/>
-        <Square coordinate={[0,1]}/>
-        <Square coordinate={[0,2]}/>
+        <Square coordinate={[0,0]} xIsNext={xIsNext} setXIsNext={setXIsNext}/>
+        <Square coordinate={[0,1]} xIsNext={xIsNext} setXIsNext={setXIsNext}/>
+        <Square coordinate={[0,2]} xIsNext={xIsNext} setXIsNext={setXIsNext}/>
         </div>
         <div className="board-row">
-        <Square coordinate={[1,0]}/>
-        <Square coordinate={[1,1]}/>
-        <Square coordinate={[1,2]}/>
+        <Square coordinate={[1,0]} xIsNext={xIsNext} setXIsNext={setXIsNext}/>
+        <Square coordinate={[1,1]} xIsNext={xIsNext} setXIsNext={setXIsNext}/>
+        <Square coordinate={[1,2]} xIsNext={xIsNext} setXIsNext={setXIsNext}/>
         </div>
         <div className="board-row">
-        <Square coordinate={[2,0]}/>
-        <Square coordinate={[2,1]}/>
-        <Square coordinate={[2,2]}/>
+        <Square coordinate={[2,0]} xIsNext={xIsNext} setXIsNext={setXIsNext}/>
+        <Square coordinate={[2,1]} xIsNext={xIsNext} setXIsNext={setXIsNext}/>
+        <Square coordinate={[2,2]} xIsNext={xIsNext} setXIsNext={setXIsNext}/>
         </div>
         </>
       );
    }
    
 
-   export function Square({coordinate, handleClick}) {
+   export function Square({coordinate, handleClick, xIsNext, setXIsNext}) {
 
     const [value, setValue] = useState(null);
+    
 
     function handleClick() {
-      console.log('Square ' + coordinate + ' was clicked!');
-      setValue("X");
+      
+      if(xIsNext) {
+        setValue("X");
+        setXIsNext(!xIsNext)
+      } else {
+        setValue("O");
+        setXIsNext(!xIsNext)
+      }
+      
     }
 
     return (
