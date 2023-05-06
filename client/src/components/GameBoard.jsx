be20c50de1b16ce68a193222508343299d3cc198
Sam Howard <samhow@colostate.edu>
2023-05-06 14:39:38 -0600

    creating gameboard

--- client/src/components/GameBoard.jsx
+++ client/src/components/GameBoard.jsx
@@ -1,11 +1,44 @@
-function GameBoard() {
+import { useState } from 'react';
+
+
+export default function GameBoard() {
     // const [count, setCount] = useState(0)
    
-     return (
-       
-      <h1>GameBoard</h1>
-   
-     )
+     return( 
+        <>
+        <h1>GameBoard</h1>
+        <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+        </div>
+        <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+        </div>
+        <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+        </div>
+        </>
+      );
    }
    
-   export default GameBoard
+
+   export function Square({handleClick}) {
+
+    const [value, setValue] = useState(null);
+
+    function handleClick() {
+      console.log('clicked!');
+      setValue("X");
+    }
+
+    return (
+      <button className="square" onClick={handleClick}>
+        {value}
+      </button>
+    );
+  }
