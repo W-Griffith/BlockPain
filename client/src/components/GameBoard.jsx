5a7352472fab261883521ef1d48ce6674cbe164e
Sam Howard <samhow@colostate.edu>
2023-05-06 15:53:40 -0600

    gameboard coorcinates!

--- client/src/components/GameBoard.jsx
+++ client/src/components/GameBoard.jsx
@@ -2,37 +2,37 @@ import { useState } from 'react';
 
 
 export default function GameBoard() {
-    // const [count, setCount] = useState(0)
+     // const [count, setCount] = useState(0)
    
      return( 
         <>
         <h1>GameBoard</h1>
         <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square coordinate={[0,0]}/>
+        <Square coordinate={[0,1]}/>
+        <Square coordinate={[0,2]}/>
         </div>
         <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square coordinate={[1,0]}/>
+        <Square coordinate={[1,1]}/>
+        <Square coordinate={[1,2]}/>
         </div>
         <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square coordinate={[2,0]}/>
+        <Square coordinate={[2,1]}/>
+        <Square coordinate={[2,2]}/>
         </div>
         </>
       );
    }
    
 
-   export function Square({handleClick}) {
+   export function Square({coordinate, handleClick}) {
 
     const [value, setValue] = useState(null);
 
     function handleClick() {
-      console.log('clicked!');
+      console.log('Square ' + coordinate + ' was clicked!');
       setValue("X");
     }
 
