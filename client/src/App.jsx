8e5fb7513ee291fcd6c307c3acd69d12502906d3
Wesh Griffith <46984407+W-Griffith@users.noreply.github.com>
2023-05-06 13:13:40 -0600

    Connecting to the contract
    Connecting solidity to react with web3

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,18 +2,22 @@
 import { Scoreboard, GameBoard, Controls, NavBar } from './components'
 import './App.css'
 
-import { ethers } from 'hardhat';
+//import { ethers } from 'hardhat';
+import Web3 from 'web3';
 import contractABI from './ticTacToeABI.json';
 
+
+
+const web3 = new Web3('http://localhost:5173'); //I'm connecting this to the same port that our local react app is running on.
 const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+const contract = new web3.eth.Contract(contractABI, contractAddress);
 
 const demoGame = async () => {
 
-  const metaMask = new ethers.providers.Web3Provider(window.ethereum);
-  const contract = await hre;
+  //const metaMask = new ethers.providers.Web3Provider(window.ethereum);
+ 
 
-  const firstGame = await contract.method.startNewGame();
-
+  const firstGame = await contract.methods.startNewGame();
   alert(firstGame);
 
 
